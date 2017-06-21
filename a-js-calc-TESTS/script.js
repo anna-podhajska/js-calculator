9cbc1f48f1ba4b9a8c3bbea6ba6f9f8c7471514b
Anna P <podhajskaa@gmail.com>
2017-06-21 14:15:29 +1200

    first commit

---
+++ a-js-calc-TESTS/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+// code code below
+
+  var equation = " ";
+
+
+  $( "div" ).click(function(event) {
+
+    $("#equation").append(" " + event.target.innerText);
+
+
+      switch (event.target.id) {
+
+        case "equals":
+
+          var result = eval(equation);
+          $("#result").html(result);
+
+          break;
+
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "plus":
+
+        equation += event.target.innerText;
+        break;
+
+
+      };
+  });
+
+
+
+// code code above
+})
---
+++ js-calculator/script.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+var equation = "";
+
+$("table").click(function (event) {
+  $("#equation").append(" " + event.target.innerText);
+
+  switch(event.target.id){
+
+    case "equals":
+      var result = eval(equation);
+      $("#result").html(result);
+      equation = "";
+      break;
+
+    case "AC":
+      equation = "";
+      $("#result").html("0");
+      break;
+
+    case "CE":
+      equation = equation.substr(0, equation.length-1);
+      //return the same str minus last element
+      break;
+
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      equation += event.target.innerText;
+      break;
+
+    case "dot":
+      if (equation.charAt(equation.length-1) != ".") {
+              equation += event.target.innerText;
+            };
+      break;
+
+    case "plus":
+      if (equation.charAt(equation.length-1) != "+") {
+        equation += event.target.innerText;
+      }
+      break;
+
+    case "minus":
+      if (equation.charAt(equation.length-1) != "-") {
+        equation += event.target.innerText;
+      }
+      break;
+
+    case "divide":
+      if (equation.charAt(equation.length-1) != "/") {
+        equation += event.target.innerText;
+      }
+      break;
+
+    case "multiply":
+      if (equation.charAt(equation.length-1) != "*") {
+        equation += event.target.innerText;
+      }
+      break;
+  }
+
+  $("#equation").html(equation);
+})
+
+// code code
+})
