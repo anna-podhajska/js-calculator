a7998d36d856d9d3954bdcc53e977fd5c2b22fc4
Anna P <podhajskaa@gmail.com>
2017-06-22 20:49:53 +1200

    color scheme function refactor

--- js-calculator/script.js
+++ js-calculator/script.js
@@ -1,77 +1,77 @@
 $(document).ready(function() {
-var equation = "";
+  var equation = "";
 
-$("table").click(function (event) {
-  $("#equation").append(" " + event.target.innerText);
+  $("table").click(function (event) {
 
-  switch(event.target.id){
+    $("#equation").append(" " + event.target.innerText);
 
-    case "equals":
+    switch(event.target.id){
 
-      try {
-        var result = eval(equation);
-        $("#result").html(result);
-        equation = "";
-        } catch (exeption) {
-            if (exeption instanceof SyntaxError) {
-              alert("invalid expression");
+      case "equals":
+        try {
+          var result = eval(equation);
+          $("#result").html(result);
+          equation = "";
+          } catch (exeption) {
+              if (exeption instanceof SyntaxError) {
+                alert("invalid expression");
+              }
             }
-          }
-      $('#equation').html("");
-      break;
+        $('#equation').html("");
+        break;
 
-    case "AC":
-      equation = "";
-      $("#result").html("0");
-      break;
+      case "AC":
+        equation = "";
+        $("#result").html("0");
+        break;
 
-    case "CE":
-      equation = equation.substr(0, equation.length-1);
-      //return the same str minus last element
-      break;
+      case "CE":
+        equation = equation.substr(0, equation.length-1);
+        //return the same str minus last element
+        break;
 
-    case "1":
-    case "2":
-    case "3":
-    case "4":
-    case "5":
-    case "6":
-    case "7":
-    case "8":
-    case "9":
-      equation += event.target.innerText;
-      break;
-
-    case "dot":
-      if (equation.charAt(equation.length-1) != ".") {
-              equation += event.target.innerText;
-            };
-      break;
-
-    case "plus":
-      if (equation.charAt(equation.length-1) != "+") {
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
         equation += event.target.innerText;
-      }
-      break;
+        break;
 
-    case "minus":
-      if (equation.charAt(equation.length-1) != "-") {
-        equation += event.target.innerText;
-      }
-      break;
+      case "dot":
+        if (equation.charAt(equation.length-1) != ".") {
+                equation += event.target.innerText;
+              };
+        break;
 
-    case "divide":
-      if (equation.charAt(equation.length-1) != "/") {
-        equation += event.target.innerText;
-      }
-      break;
+      case "plus":
+        if (equation.charAt(equation.length-1) != "+") {
+          equation += event.target.innerText;
+        }
+        break;
 
-    case "multiply":
-      if (equation.charAt(equation.length-1) != "*") {
-        equation += event.target.innerText;
-      }
-      break;
-  }  // switch end
+      case "minus":
+        if (equation.charAt(equation.length-1) != "-") {
+          equation += event.target.innerText;
+        }
+        break;
+
+      case "divide":
+        if (equation.charAt(equation.length-1) != "/") {
+          equation += event.target.innerText;
+        }
+        break;
+
+      case "multiply":
+        if (equation.charAt(equation.length-1) != "*") {
+          equation += event.target.innerText;
+        }
+        break;
+    }  // switch end
 
   if (equation.length > 35 ) {
     alert("really? hey, shorter equation is actionable");
@@ -80,11 +80,35 @@ $("table").click(function (event) {
   $("#equation").html(equation);
 })
 
+// MY COLOR VERSIONS:
+$("#moreFun").click(colorColorScheme);
+$("#lessFun").click(classicColorsScheme);
+
+// doc load end:
+});
 
 
-// MY COLOR VERSIONS ON:
+// CLASSIC COLOR SCHEME FUNCTION:
+function classicColorsScheme() {
+  $("#lessFun").css('background-color', '#f1c689');
+  $("#moreFun").css('background-color', '#4b5160');
 
-$("#moreFun").click(function () {
+  $(".op1").css('background-color','');
+  $('.op1').unbind();
+
+  $("#AC, #CE").css('background-color','');
+  $("#AC, #CE").unbind();
+
+  $("th").css('font-family', '');
+  $("th").css('background-color','');
+  $("th").css('color', '');
+
+  $("#equation").css('color', '');
+};
+
+
+//COLOR SCHEME FUNCTION:
+function colorColorScheme() {
 
   $("#moreFun").css('background-color', '#f1c689');
   $("#lessFun").css('background-color', '#4b5160');
@@ -106,38 +130,4 @@ $("#moreFun").click(function () {
     $(this).css('background-color','#f1c689');
   });
 
-});
-
-// COLOR VERSIONS CLEANUP:
-
-$("#lessFun").click(function () {
-
-  $("#lessFun").css('background-color', '#f1c689');
-  $("#moreFun").css('background-color', '#4b5160');
-
-  $(".op1").css('background-color','');
-  $('.op1').unbind();
-
-  $("#AC, #CE").css('background-color','');
-  $("#AC, #CE").unbind();
-
-  $("th").css('font-family', '');
-  $("th").css('background-color','');
-  $("th").css('color', '');
-
-  $("#equation").css('color', '');
-
-  });
-
-  // sound
-  // function play() {
-  //   var audioElement = document.createElement('audio');
-  //       audioElement.setAttribute('src', 'sounds/click.wav');
-  //   audioElement.play();
-    // }
-  // sound
-
-
-
-// code code
-})
+};
